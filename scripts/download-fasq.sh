1947a0065df7eb21ae6519eaacf0750b00e4e9c5
Emre Ovet <eovet@dons.usfca.edu>
2018-12-05 14:01:55 -0800

    Correct shell typos.

--- scripts/download-fasq.sh
+++ scripts/download-fasq.sh
@@ -9,5 +9,5 @@
 
 for SRA_number in $(cut -f 6 data/metadata/SraRunTable.txt | tail -n +2)
 do
-  /Users/emreovet/sratoolkit.2.9.2-mac64/bin/fastq-dump -v $SRA_number -O data/raw_data
+  /Users/emreovet/sratoolkit.2.9.2-mac64/bin/fastq-dump -v "$SRA_number" -O data/raw_data
 done
